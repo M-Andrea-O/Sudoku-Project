2a939bcafa6d9de286055cb13e1b52256edb6554
Mike <shanou0601@gmail.com>
2025-11-29 17:37:53 -0500

    Create Board_Class.py

---
+++ Board_Class.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.init()
+class Board:
+    def __init__(self, width, height, screen, difficulty):
+
+    def draw(self):
+
+    def select(self, row, col):
+
+    def clear(self):
+
+    def sketch(self,value):
+
+    def place_number(self,value):
+
+    def rest_to_origin(self):
+
+    def is_full(self):
+
+    def update_board(self):
+
+    def find_empty(self):
+
+    def check_board(self):
+
+
