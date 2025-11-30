d624e4187ce979c84e77f9122dfeb17d9f51f80f
yasminT-shipit <ytatari123@gmail.com>
2025-11-29 16:25:18 -0800

    Update Board_Class.py

--- Board_Class.py
+++ Board_Class.py
@@ -58,8 +58,21 @@ class Board:
         self.cells[row][col].selected = True
 
     def clear(self):
+        """Clear the selected cell's value if it's editable"""
+        if self.selected:
+            row, col = self.selected
+            # clears only if the cell wasn't part of the original puzzle
+            if self.cells[row][col].value == 0 or self.cells[row][col].sketched != 0:
+                self.cells[row][col].set_cell_value(0)
+                self.cells[row][col].set_sketched_value(0)
 
-    def sketch(self,value):
+    def sketch(self, value):
+        """Set the sketched value of the selected cell"""
+        if self.selected and 1 <= value <= 9:
+            row, col = self.selected
+            #allows sketching only if the cell is editable
+            if self.cells[row][col].value == 0:
+                self.cells[row][col].set_sketched_value(value)
 
     def place_number(self,value):
 
